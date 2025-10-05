dac5269f8e920681162e53538084674bcbda1db6
Brayden Messerschmidt <minecreeper0913@gmail.com>
2025-10-05 18:07:11 -0500

    update memory usage

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java -Xmx6G @libraries/net/minecraftforge/forge/1.20.1-47.4.0/unix_args.txt --nogui "$@"
+java -Xmx12G @libraries/net/minecraftforge/forge/1.20.1-47.4.0/unix_args.txt --nogui "$@"
