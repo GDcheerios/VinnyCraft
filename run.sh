caa33b6734f4f81b3222aa75a3874aa02915ae12
Brayden Messerschmidt <minecreeper0913@gmail.com>
2025-10-05 21:14:08 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,13 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java -Xmx12G @libraries/net/minecraftforge/forge/1.20.1-47.4.0/unix_args.txt --nogui "$@"
+#!/usr/bin/env sh
+java -Xms8G -Xmx12G \
+     -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=100 \
+     -XX:+UnlockExperimentalVMOptions -XX:+AlwaysPreTouch -XX:+DisableExplicitGC \
+     -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M \
+     -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 \
+     -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 \
+     -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 \
+     @libraries/net/minecraftforge/forge/1.20.1-47.4.0/unix_args.txt "$@"
+
