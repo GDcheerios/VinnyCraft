2d71484956c9ba64f98471590ecf91916897f584
Brayden Messerschmidt <minecreeper0913@gmail.com>
2025-10-21 13:15:17 -0500

    update startup script

--- run.bat
+++ run.bat
@@ -1,4 +1,44 @@
 @echo off
 title VinnyCraft
-java -Xms8G -Xmx12G -jar server.jar nogui
+
+set "serverURL=https://mohistmc.com/api/v2/projects/mohist/versions/1.20.1/builds/latest/download"
+set "PPURL=https://github.com/GDcheerios/PointPlugin/releases/download/v0.0.1/PointPlugin-1.0-SNAPSHOT.jar"
+set "MVIURL=https://cdn.modrinth.com/data/qvdtDX3s/versions/MeYBNg6N/multiverse-inventories-5.2.0.jar"
+set "MVCURL=https://cdn.modrinth.com/data/3wmN97b8/versions/K4FBldrK/multiverse-core-5.3.3.jar"
+
+set "serverOUT=server.jar"
+set "PPOUT=plugins\PointPlugin.jar"
+set "MVIOUT=plugins\multiverse-inventory.jar"
+set "MVCOUT=plugins\multiverse-core.jar"
+
+echo Checking for required files...
+if not exist "plugins" (
+    echo Creating plugins folder...
+    mkdir "plugins"
+)
+
+if not exist "%serverOUT%" (
+    echo Downloading server.jar...
+    curl --fail --location --output "%serverOUT%" "%serverURL%"
+)
+
+if not exist "%PPOUT%" (
+    echo Downloading PointPlugin.jar...
+    curl --fail --location --output "%PPOUT%" "%PPURL%"
+)
+
+if not exist "%MVIOUT%" (
+    echo Downloading multiverse-inventory.jar...
+    curl --fail --location --output "%MVIOUT%" "%MVIURL%"
+)
+
+if not exist "%MVCOUT%" (
+    echo Downloading multiverse-core.jar...
+    curl --fail --location --output "%MVCOUT%" "%MVCURL%"
+)
+
+echo.
+echo Starting server...
+java -Xms8G -Xmx12G -jar "%serverOUT%" nogui
+
 pause
--- run.sh
+++ run.sh
@@ -1,2 +1,40 @@
-#!/usr/bin/env bash
-java -Xms8G -Xmx12G -jar server.jar nogui
+set -e
+
+serverURL="https://mohistmc.com/api/v2/projects/mohist/versions/1.20.1/builds/latest/download"
+PPURL="https://github.com/GDcheerios/PointPlugin/releases/download/v0.0.1/PointPlugin-1.0-SNAPSHOT.jar"
+MVIURL="https://cdn.modrinth.com/data/qvdtDX3s/versions/MeYBNg6N/multiverse-inventories-5.2.0.jar"
+MVCURL="https://cdn.modrinth.com/data/3wmN97b8/versions/K4FBldrK/multiverse-core-5.3.3.jar"
+
+serverOUT="server.jar"
+PPOUT="plugins/PointPlugin.jar"
+MVIOUT="plugins/multiverse-inventory.jar"
+MVCOUT="plugins/multiverse-core.jar"
+
+echo "=== VinnyCraft Setup ==="
+
+if [ ! -d "plugins" ]; then
+    echo "Creating plugins folder..."
+    mkdir -p "plugins"
+fi
+
+download_if_missing() {
+    local file="$1"
+    local url="$2"
+    local name="$3"
+
+    if [ ! -f "$file" ]; then
+        echo "Downloading $name..."
+        curl --fail --location --output "$file" "$url"
+    else
+        echo "$name already exists, skipping."
+    fi
+}
+
+download_if_missing "$serverOUT" "$serverURL" "server.jar"
+download_if_missing "$PPOUT" "$PPURL" "PointPlugin.jar"
+download_if_missing "$MVIOUT" "$MVIURL" "multiverse-inventory.jar"
+download_if_missing "$MVCOUT" "$MVCURL" "multiverse-core.jar"
+
+echo
+echo "=== Starting server ==="
+java -Xms8G -Xmx12G -jar "$serverOUT" nogui
